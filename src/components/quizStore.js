bcb8fe645e776db60a43ffd2fe03f0fa154af84d
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-10-04 22:26:41 +0200

    Update quizStore.js

--- src/components/quizStore.js
+++ src/components/quizStore.js
@@ -1,8 +1,24 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 
-export const isQuizMode = writable(browser ? localStorage.getItem('isQuizMode') === 'true' : false);
+const createIsQuizMode = () => {
+  const { subscribe, set, update } = writable(false);
 
-if (browser) {
-  isQuizMode.subscribe(value => localStorage.setItem('isQuizMode', value));
-}
+  if (browser) {
+    const storedValue = localStorage.getItem('isQuizMode') === 'true';
+    set(storedValue);
+  }
+
+  return {
+    subscribe,
+    set: (value) => {
+      if (browser) {
+        localStorage.setItem('isQuizMode', value);
+      }
+      set(value);
+    },
+    toggle: () => update(n => !n)
+  };
+};
+
+export const isQuizMode = createIsQuizMode();
