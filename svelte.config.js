b22048057e51e75d7113c16e6e39af28643e0600
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-10-04 16:06:52 +0200

    remove subdirectory

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import vercel from '@sveltejs/adapter-vercel';
+import { vitePreprocess} from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,6 +8,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: vercel()
+	},
+
+	preprocess: vitePreprocess(),
+	vite: {
+		ssr: {
+			noExternal: ['gsap']
+		}
 	}
 };
 
