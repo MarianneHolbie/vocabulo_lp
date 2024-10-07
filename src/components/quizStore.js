b415916ab4052e6c0ccd5669ebe2cf7090c80629
MarianneHolbie <112870394+MarianneHolbie@users.noreply.github.com>
2024-10-07 11:01:58 +0200

    encapsuled localstorage in broswer check

--- src/components/quizStore.js
+++ src/components/quizStore.js
@@ -1,33 +1,6 @@
+// Import the writable function from Svelte's store module
 import { writable } from 'svelte/store';
-import { browser } from '$app/environment';
 
-const createIsQuizMode = () => {
-  const { subscribe, set, update } = writable(false);
-
-  if (browser) {
-    const storedValue = localStorage.getItem('isQuizMode') === 'true';
-    set(storedValue);
-  }
-
-  return {
-    subscribe,
-    set: (value) => {
-      if (browser) {
-        localStorage.setItem('isQuizMode', value);
-      }
-      set(value);
-    },
-    toggle: () => {
-      if (browser) {
-        update(n => {
-          localStorage.setItem('isQuizMode', !n);
-          return !n;
-        });
-      } else {
-        update(n => !n);
-      }
-    }
-  };
-};
-
-export const isQuizMode = createIsQuizMode();
+// Create and export a writable store named 'isQuizMode'
+// Initialize it with a default value of true
+export const isQuizMode = writable(true);
